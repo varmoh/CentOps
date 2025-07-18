f0187d5cf16084fc360bf8ec8078bb61f7326188
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-18 12:37:32 +0300

    Update menuData.tsx

--- GUI/src/components/MenuTree/menuData.tsx
+++ GUI/src/components/MenuTree/menuData.tsx
@@ -4,30 +4,32 @@ import {
   MdOutlineForum,
 } from 'react-icons/md';
 
+const env = import.meta.env;
+
 export const menuData = [
   {
     id: 'clients',
     icon: <MdOutlineForum className="menu-item-icon" />,
-    url: '/clients',
+    url: `/${env.REACT_APP_PROJECT_LAYER}/clients`,
   },
   {
     id: 'users',
     icon: <MdOutlineAdb className="menu-item-icon" />,
-    url: '/users',
+    url: `/${env.REACT_APP_PROJECT_LAYER}/users`,
   },
   {
     id: 'clusters',
     icon: <MdOutlineEqualizer className="menu-item-icon" />,
-    url: '/clusters',
+    url: `/${env.REACT_APP_PROJECT_LAYER}/clusters`,
   },
   {
     id: 'audit',
     icon: <MdOutlineForum className="menu-item-icon" />,
-    url: '/audit',
+    url: `/${env.REACT_APP_PROJECT_LAYER}/audit`,
   },
   {
     id: 'documentation',
     icon: <MdOutlineForum className="menu-item-icon" />,
-    url: '/documentation',
+    url: `/${env.REACT_APP_PROJECT_LAYER}/documentation`,
   },
 ];
