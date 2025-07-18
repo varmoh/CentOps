30c74a8a9d87a56a0532d02425f10a8dcf74f888
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-18 12:37:07 +0300

    Update index.tsx

--- GUI/src/components/MenuTree/index.tsx
+++ GUI/src/components/MenuTree/index.tsx
@@ -42,67 +42,75 @@ export const MenuTree: FC<MenuTreeProps> = ({
     .filter((x) => !x.hidden)
     .map((menuItem) => (
       <li key={menuItem.label[currentlySelectedLanguage]}>
-        {menuItem.children ? (
-          <>
-            <button
-              className={clsx('nav__toggle', {
-                'nav__toggle--icon': !!menuItem.id,
-              })}
-              aria-expanded={
-                menuItem.path && isSameRoot(menuItem, serviceId)
-                  ? 'true'
-                  : 'false'
-              }
-              onClick={handleNavToggle}
-            >
-              {menuItem.id && (
-                <Icon
-                  icon={
-                    menuData.find((dataItem) => dataItem.id === menuItem.id)
-                      ?.icon
+        {(() => {
+          if (menuItem.children) {
+            return (
+              <>
+                <button
+                  className={clsx('nav__toggle', {
+                    'nav__toggle--icon': !!menuItem.id,
+                  })}
+                  aria-expanded={
+                    menuItem.path && isSameRoot(menuItem, serviceId)
+                      ? 'true'
+                      : 'false'
                   }
-                  size="large"
+                  onClick={handleNavToggle}
+                >
+                  {menuItem.id && (
+                    <Icon
+                      icon={
+                        menuData.find((dataItem) => dataItem.id === menuItem.id)
+                          ?.icon
+                      }
+                      size="large"
+                    />
+                  )}
+                  <span className="menu-item-title">
+                    {menuItem.label[currentlySelectedLanguage]}
+                  </span>
+                  <Icon
+                    icon={<MdKeyboardArrowDown />}
+                    className="menu-item-arrow"
+                  />
+                </button>
+                <ul className="nav__submenu">
+                  <MenuTree
+                    menuItems={menuItem.children.map((item) => ({
+                      id: menuItem.id,
+                      ...item,
+                    }))}
+                    serviceId={serviceId}
+                    handleNavToggle={handleNavToggle}
+                  />
+                </ul>
+              </>
+            );
+          } else if (serviceId.includes(menuItem.id!)) {
+            return (
+              <NavLink to={menuItem.path || '#'}>
+                <MenuItemLabel
+                  menuItem={menuItem}
+                  currentlySelectedLanguage={currentlySelectedLanguage}
                 />
-              )}
-              <span className="menu-item-title">
-                {menuItem.label[currentlySelectedLanguage]}
-              </span>
-              <Icon
-                icon={<MdKeyboardArrowDown />}
-                className="menu-item-arrow"
-              />
-            </button>
-            <ul className="nav__submenu">
-              <MenuTree
-                menuItems={menuItem.children.map((item) => ({
-                  id: menuItem.id,
-                  ...item,
-                }))}
-                serviceId={serviceId}
-                handleNavToggle={handleNavToggle}
-              />
-            </ul>
-          </>
-        ) : serviceId.includes(menuItem.id!) ? (
-          <NavLink to={menuItem.path || '#'}>
-            <MenuItemLabel
-              menuItem={menuItem}
-              currentlySelectedLanguage={currentlySelectedLanguage}
-            />
-          </NavLink>
-        ) : (
-          <a
-            href={
-              (menuData.find((dataItem) => dataItem.id === menuItem.id)?.url ??
-                '') + menuItem.path
-            }
-          >
-            <MenuItemLabel
-              menuItem={menuItem}
-              currentlySelectedLanguage={currentlySelectedLanguage}
-            />
-          </a>
-        )}
+              </NavLink>
+            );
+          } else {
+            return (
+              <a
+                href={
+                  (menuData.find((dataItem) => dataItem.id === menuItem.id)
+                    ?.url ?? '') + menuItem.path
+                }
+              >
+                <MenuItemLabel
+                  menuItem={menuItem}
+                  currentlySelectedLanguage={currentlySelectedLanguage}
+                />
+              </a>
+            );
+          }
+        })()}
       </li>
     ));
 };
