85f02e36feb38ea046024c659c0cb91ab92ee879
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 14:57:58 +0300

    Update update-client.sql

--- DSL/Resql/centops/POST/update-client.sql
+++ DSL/Resql/centops/POST/update-client.sql
@@ -3,17 +3,17 @@ INSERT INTO clients (client_id,
                      kubernetes_cluster_address,
                      kubernetes_cluster_namespace,
                      argo_app_deployment_name,
-                     authentication_certificate,
                      updated_at,
-                     created_at)
+                     created_at,
+                     part_of_network)
 SELECT client_id,
        :name,
        :kubernetes_cluster_address,
        :kubernetes_cluster_namespace,
        :argo_app_deployment_name,
-       :authentication_certificate,
        now(),
-       created_at
+       created_at,
+       :part_of_network
 FROM clients
 WHERE client_id = :client_id::uuid
   AND id = (SELECT max (id) FROM clients WHERE client_id = :client_id::uuid);
