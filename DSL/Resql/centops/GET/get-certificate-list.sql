c4fead010f82e036ac581b21f4bf1885ca66e91b
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 10:50:36 +0300

    Update get-certificate-list.sql

--- DSL/Resql/centops/GET/get-certificate-list.sql
+++ DSL/Resql/centops/GET/get-certificate-list.sql
@@ -1,10 +1,11 @@
 SELECT certificate_id,
        client_id,
        created_at,
+       revoked,
        deleted,
        CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM certificates c
-WHERE c.client_id = :client_id::uuid
+WHERE c.client_id = :client_id::uuid AND deleted=false
   AND c.id = (SELECT max(id) FROM certificates WHERE certificate_id = c.certificate_id)
 ORDER BY id
 OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
