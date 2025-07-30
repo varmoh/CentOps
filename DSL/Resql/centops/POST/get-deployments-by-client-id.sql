b7e4466c01a90b393c9df0459da38b47d7ecb6fe
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 10:51:52 +0300

    Update get-deployments-by-client-id.sql

--- DSL/Resql/centops/POST/get-deployments-by-client-id.sql
+++ DSL/Resql/centops/POST/get-deployments-by-client-id.sql
@@ -5,7 +5,9 @@ SELECT id,
        argo_deploy_app_name,
        concat(deployed_by_username, ' ', deployed_by_lastname) as deployed_by,
        status,
-       created_at
+       created_at,
+       CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM deployments
 WHERE client_id = :client_id::uuid
-ORDER BY created_at DESC;
+ORDER BY created_at DESC
+OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
