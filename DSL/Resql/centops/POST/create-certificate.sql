9eb3b2bb8717bf1b09e5c041b019b44ce5d053bb
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 10:51:03 +0300

    Update create-certificate.sql

--- DSL/Resql/centops/POST/create-certificate.sql
+++ DSL/Resql/centops/POST/create-certificate.sql
@@ -1,3 +1,16 @@
-INSERT INTO certificates (client_id, public_key)
-VALUES (:client_id::uuid, :public_key)
-RETURNING certificate_id;
+WITH existing AS (SELECT certificate_id
+                  FROM certificates
+                  WHERE client_id = :client_id::uuid
+                    AND revoked = false
+                    AND deleted = false
+                  LIMIT 1),
+     revoked AS (
+         UPDATE certificates
+             SET revoked = true
+             WHERE certificate_id = (SELECT certificate_id FROM existing)),
+     inserted AS (
+         INSERT INTO certificates (client_id, public_key)
+             VALUES (:client_id::uuid, :public_key)
+             RETURNING certificate_id)
+SELECT certificate_id
+FROM inserted;
