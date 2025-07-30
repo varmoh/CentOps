64b6e382eb5c4e71c8f1da1865498a95a6cb1d9d
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 08:32:58 +0300

    Update get-manifests-by-client-id.sql

--- DSL/Resql/centops/POST/get-manifests-by-client-id.sql
+++ DSL/Resql/centops/POST/get-manifests-by-client-id.sql
@@ -3,9 +3,11 @@ SELECT manifest_id,
        name,
        helm_version,
        created_at,
-       updated_at
+       updated_at,
+       CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM manifests
 WHERE client_id = :client_id::uuid
   AND manifest_id IN (SELECT max(manifest_id) from manifests GROUP BY helm_version)
   AND deleted = FALSE
-ORDER BY created_at DESC;
+ORDER BY created_at DESC
+OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
