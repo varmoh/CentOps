9a0bf7a0ee01d2087677f1ac4d0cd6db800b4329
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 14:56:20 +0300

    Update get-user-log-list.sql

--- DSL/Resql/centops/GET/get-user-log-list.sql
+++ DSL/Resql/centops/GET/get-user-log-list.sql
@@ -3,9 +3,12 @@ SELECT
     method,
     path,
     created_at,
+    user_id_code,
+    first_name,
+    last_name,
     CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM user_logs l
 WHERE l.id = (SELECT max(id) FROM user_logs WHERE log_id = l.log_id)
   AND l.deleted = false
-ORDER BY id
+ORDER BY created_at desc
 OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
