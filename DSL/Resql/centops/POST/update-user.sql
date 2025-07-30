19c1624eee6f9e594410ab631343e0294f73a4af
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 14:58:35 +0300

    Update update-user.sql

--- DSL/Resql/centops/POST/update-user.sql
+++ DSL/Resql/centops/POST/update-user.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (user_id, first_name, last_name, id_code)
-SELECT user_id, :first_name, :last_name, :id_code
+INSERT INTO users (user_id, first_name, last_name, id_code, updated_at, created_at)
+SELECT user_id, :first_name, :last_name, :id_code, now(), created_at
 FROM users
 WHERE user_id = :user_id::uuid
 AND id IN (SELECT max(id) FROM users WHERE user_id = :user_id::uuid GROUP BY user_id);
