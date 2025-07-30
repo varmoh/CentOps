90c2dd9ca0c27bd8a12f59aca262ee5a62cbee7a
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 14:56:38 +0300

    Update get-user.sql

--- DSL/Resql/centops/GET/get-user.sql
+++ DSL/Resql/centops/GET/get-user.sql
@@ -2,7 +2,8 @@ SELECT user_id,
        first_name,
        last_name,
        id_code,
-       created_at
+       created_at,
+       updated_at
 FROM users
 WHERE user_id = :user_id::uuid
 AND id IN (SELECT max(id) FROM users WHERE user_id = :user_id::uuid GROUP BY user_id)
