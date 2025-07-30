0d6431cdd7cb3e3b82fd78c637bb2949b2ae3171
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 14:58:15 +0300

    Update update-cluster.sql

--- DSL/Resql/centops/POST/update-cluster.sql
+++ DSL/Resql/centops/POST/update-cluster.sql
@@ -1,5 +1,5 @@
-INSERT INTO clusters (cluster_id, name, ip_address)
-SELECT cluster_id, :name, :ip_address
+INSERT INTO clusters (cluster_id, name, ip_address, updated_at, created_at)
+SELECT cluster_id, :name, :ip_address, now(), created_at
 FROM clusters
 WHERE cluster_id = :cluster_id::uuid
   AND id = (SELECT max(id) FROM clusters WHERE cluster_id = :cluster_id::uuid);
