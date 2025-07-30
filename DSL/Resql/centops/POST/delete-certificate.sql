8577950465a984791dfee82c22f1f4d70e57cf2e
Varmo <101868197+varmoh@users.noreply.github.com>
2025-07-30 10:51:24 +0300

    Update delete-certificate.sql

--- DSL/Resql/centops/POST/delete-certificate.sql
+++ DSL/Resql/centops/POST/delete-certificate.sql
@@ -1,7 +1,8 @@
-INSERT INTO certificates (certificate_id, client_id, public_key, deleted)
+INSERT INTO certificates (certificate_id, client_id, public_key, revoked, deleted)
 SELECT certificate_id,
        client_id,
        public_key,
+       TRUE,
        TRUE
 FROM certificates
 WHERE certificate_id = :certificate_id::uuid
