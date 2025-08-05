38bb628e52a166cf1fd3ecfe6691698301013229
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-05 08:58:46 +0300

    Update log-vault-api-get-delete-success-action.sql

--- DSL/Resql/centops/POST/log-vault-api-get-delete-success-action.sql
+++ DSL/Resql/centops/POST/log-vault-api-get-delete-success-action.sql
@@ -3,10 +3,20 @@ INSERT INTO vault_api_action_log (user_id_code,
                                   action,
                                   secret_key,
                                   status_code,
-                                  user_agent)
+                                  user_agent,
+                                  first_name,
+                                  last_name,
+                                  client_name)
 VALUES (:user_id_code,
         :ip_address,
         :action,
         :secret_key,
         :status_code,
-        :user_agent) RETURNING id, created_at;
+        :user_agent,
+        :first_name,
+        :last_name,
+        (SELECT name
+         FROM clients
+         WHERE client_id = :client_id::uuid
+            AND id IN (SELECT max (id) FROM clients GROUP BY client_id)
+            AND deleted = FALSE)) RETURNING id, created_at;
