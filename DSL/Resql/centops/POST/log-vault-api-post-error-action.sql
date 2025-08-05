e23ce52abcc83859d77e4172361cfa933cb36e41
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-05 08:59:11 +0300

    Update log-vault-api-post-error-action.sql

--- DSL/Resql/centops/POST/log-vault-api-post-error-action.sql
+++ DSL/Resql/centops/POST/log-vault-api-post-error-action.sql
@@ -4,11 +4,21 @@ INSERT INTO vault_api_action_log (user_id_code,
                                   secret_key,
                                   status_code,
                                   error_message,
-                                  user_agent)
+                                  user_agent,
+                                  first_name,
+                                  last_name,
+                                  client_name)
 VALUES (:user_id_code,
         :ip_address,
         :action,
         :secret_key,
         :status_code,
         :error_message,
-        :user_agent) RETURNING id, created_at;
+        :user_agent,
+        :first_name,
+        :last_name,
+        (SELECT name
+         FROM clients
+         WHERE client_id = :client_id::uuid
+            AND id IN (SELECT max (id) FROM clients GROUP BY client_id)
+            AND deleted = FALSE)) RETURNING id, created_at;
