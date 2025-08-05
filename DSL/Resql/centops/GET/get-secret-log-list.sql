5b5e1884705fd6898487b526c62d5d63303bd196
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-05 08:57:23 +0300

    Update get-secret-log-list.sql

--- DSL/Resql/centops/GET/get-secret-log-list.sql
+++ DSL/Resql/centops/GET/get-secret-log-list.sql
@@ -5,6 +5,9 @@ SELECT
     ip_address,
     action,
     user_agent,
+    first_name,
+    last_name,
+    client_name,
     CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM vault_api_action_log
 ORDER BY id
