4e26241c16ede60a5086d79cbccab9d07a4a7ef1
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-06 06:44:51 +0300

    Update get-secret-log-list.sql

--- DSL/Resql/centops/GET/get-secret-log-list.sql
+++ DSL/Resql/centops/GET/get-secret-log-list.sql
@@ -10,5 +10,5 @@ SELECT
     client_name,
     CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM vault_api_action_log
-ORDER BY id
+ORDER BY created_at desc
 OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
