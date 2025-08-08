469dbc9d54ad6dfedae3ac94d252086428db13d5
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-08 06:36:04 +0300

    Update get-client-by-id.sql

--- DSL/Resql/centops/GET/get-client-by-id.sql
+++ DSL/Resql/centops/GET/get-client-by-id.sql
@@ -1,6 +1,7 @@
 SELECT client_id,
        name,
        kubernetes_cluster_namespace,
+       kubernetes_cluster_address,
        argo_app_deployment_name
 FROM clients
 WHERE client_id = :client_id::uuid
