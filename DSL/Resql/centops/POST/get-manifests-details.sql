8260a2f0ee2ed1bceedd388c36234eb2e36a744d
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-12 10:13:37 +0300

    Update get-manifests-details.sql

--- DSL/Resql/centops/POST/get-manifests-details.sql
+++ DSL/Resql/centops/POST/get-manifests-details.sql
@@ -7,5 +7,5 @@ SELECT manifest_id,
        updated_at
 FROM manifests
 WHERE client_id = :client_id::uuid AND manifest_id = CAST(:manifest_id AS BIGINT)
-  AND manifest_id IN (SELECT max(manifest_id) from manifests GROUP BY helm_version)
+  AND manifest_id IN (SELECT max(manifest_id) from manifests GROUP BY name)
   AND deleted = FALSE;
