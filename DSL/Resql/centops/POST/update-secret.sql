c710c458be8dc56bcc7be24f9f8e0406944f43cc
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-12 10:12:17 +0300

    Update update-secret.sql

--- DSL/Resql/centops/POST/update-secret.sql
+++ DSL/Resql/centops/POST/update-secret.sql
@@ -1,7 +1,10 @@
-WITH origin AS (SELECT *
-                FROM secrets
-                WHERE id = CAST(:id AS BIGINT)
-                  AND client_id = :client_id::uuid),
+WITH origin AS (
+    UPDATE secrets
+        SET deleted = true
+        WHERE id = CAST(:id AS BIGINT)
+        AND client_id = :client_id::uuid
+        RETURNING name, environment, created_at
+),
      inserted AS (
          INSERT INTO secrets (client_id, name, environment, created_at, updated_at)
              SELECT :client_id::uuid,
