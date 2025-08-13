5b78636df0ff22f204d8a5aa488e64ee7d0b05f0
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-13 11:07:43 +0300

    Update vite.config.ts

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -19,12 +19,6 @@ export default defineConfig({
     port: 8057,
     host: '0.0.0.0',
     allowedHosts: true,
-    proxy: {
-      '/centops': {
-        target: 'https://ruuter.centops.test.buerokratt.ee',
-        changeOrigin: true,
-      },
-    },
   },
   plugins: [
     react(),
