f524e72b19711e245b480862baccaf9f211e2acf
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:52:50 +0300

    Update create-failed-deployment.sql

--- DSL/Resql/centops/POST/create-failed-deployment.sql
+++ DSL/Resql/centops/POST/create-failed-deployment.sql
@@ -1,5 +1,5 @@
-INSERT INTO deployments (client_id, manifest_id, manifest_version, status, deployed_by_id_code,
+INSERT INTO deployments (client_id, manifest_id, manifest_name, manifest_git_helm_branch, status, deployed_by_id_code,
                          deployed_by_username, deployed_by_lastname, argo_deploy_app_name, argo_deploy_err_message)
-VALUES (:client_id::uuid, :manifest_id, :manifest_version, :status, :deployed_by_id_code,
+VALUES (:client_id::uuid, :manifest_id::uuid, :manifest_name, :manifest_git_helm_branch, :status, :deployed_by_id_code,
         :deployed_by_username, :deployed_by_lastname, :argo_deploy_app_name, :argo_deploy_err_message)
 RETURNING id, status;
