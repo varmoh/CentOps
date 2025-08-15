a4b2f1d7faff287002bef24524b151aea62fcf99
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:54:52 +0300

    Update get-manifests-by-client-id.sql

--- DSL/Resql/centops/POST/get-manifests-by-client-id.sql
+++ DSL/Resql/centops/POST/get-manifests-by-client-id.sql
@@ -1,14 +1,16 @@
 SELECT manifest_id,
        client_id,
        name,
-       helm_version,
+       git_helm_branch,
        created_at,
        updated_at,
        deployed_at,
+       git_helm_repository,
+       git_helm_path,
        CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM manifests
 WHERE client_id = :client_id::uuid
-  AND manifest_id IN (SELECT max(manifest_id) from manifests GROUP BY name)
+  AND id IN (SELECT max(id) from manifests GROUP BY name)
   AND deleted = FALSE
 ORDER BY created_at DESC
 OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
