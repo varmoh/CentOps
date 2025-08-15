cf265476ff1e2df45c94c6694dc8d61a0aa3a29d
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:51:59 +0300

    Update create-client.sql

--- DSL/Resql/centops/POST/create-client.sql
+++ DSL/Resql/centops/POST/create-client.sql
@@ -1,10 +1,10 @@
 INSERT INTO clients (name,
-                     kubernetes_cluster_address,
+                     kubernetes_cluster_id,
                      kubernetes_cluster_namespace,
                      argo_app_deployment_name,
                      part_of_network)
 VALUES (:name,
-        :kubernetes_cluster_address,
+        :kubernetes_cluster_id::uuid,
         :kubernetes_cluster_namespace,
         :argo_app_deployment_name,
         :part_of_network)
