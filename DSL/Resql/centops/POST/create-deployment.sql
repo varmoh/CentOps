bc1e77e89376a98a0f9617eb8bf42867d2634e8a
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:52:29 +0300

    Update create-deployment.sql

--- DSL/Resql/centops/POST/create-deployment.sql
+++ DSL/Resql/centops/POST/create-deployment.sql
@@ -1,5 +1,5 @@
-INSERT INTO deployments (client_id, manifest_id, manifest_version, status, deployed_by_id_code,
+INSERT INTO deployments (client_id, manifest_id, manifest_name, manifest_git_helm_branch, status, deployed_by_id_code,
                          deployed_by_username, deployed_by_lastname, argo_deploy_app_name, argo_deployment_id)
-VALUES (:client_id::uuid, :manifest_id, :manifest_version, :status, :deployed_by_id_code,
+VALUES (:client_id::uuid, :manifest_id::uuid, :manifest_name, :manifest_git_helm_branch, :status, :deployed_by_id_code,
         :deployed_by_username, :deployed_by_lastname, :argo_deploy_app_name,CAST(:argo_deployment_id AS UUID))
 RETURNING id, status;
