809d6e0b1a497f782bec8153fc915af7bcc5346b
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:53:36 +0300

    Update delete-manifest.sql

--- DSL/Resql/centops/POST/delete-manifest.sql
+++ DSL/Resql/centops/POST/delete-manifest.sql
@@ -1,15 +1,21 @@
 INSERT INTO manifests (name,
                        client_id,
-                       helm_version,
+                       manifest_id,
+                       git_helm_branch,
                        helm_values,
                        deleted,
-                       created_at)
+                       created_at,
+                       git_helm_repository,
+                       git_helm_path
+)
 SELECT name,
        client_id,
-       helm_version,
+       manifest_id,
+       git_helm_branch,
        helm_values,
        TRUE,
-       NOW()
+       NOW(),
+       git_helm_repository,
+       git_helm_path
 FROM manifests
-WHERE manifest_id = CAST(:manifest_id AS BIGINT) AND client_id = :client_id::uuid
-
+WHERE manifest_id = :manifest_id::uuid AND client_id = :client_id::uuid;
