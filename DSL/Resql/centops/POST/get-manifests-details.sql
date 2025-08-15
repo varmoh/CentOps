7daa3e1f6f4d4bf1b131a9174ed6c5ed579c0a9f
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:55:16 +0300

    Update get-manifests-details.sql

--- DSL/Resql/centops/POST/get-manifests-details.sql
+++ DSL/Resql/centops/POST/get-manifests-details.sql
@@ -1,11 +1,13 @@
 SELECT manifest_id,
        name,
        client_id,
-       helm_version,
+       git_helm_branch,
        helm_values,
        created_at,
-       updated_at
+       updated_at,
+       git_helm_repository,
+       git_helm_path
 FROM manifests
-WHERE client_id = :client_id::uuid AND manifest_id = CAST(:manifest_id AS BIGINT)
-  AND manifest_id IN (SELECT max(manifest_id) from manifests GROUP BY name)
+WHERE client_id = :client_id::uuid AND manifest_id = :manifest_id::uuid
+  AND id IN (SELECT max(id) from manifests GROUP BY name)
   AND deleted = FALSE;
