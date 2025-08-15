eab60a76e61df79437131b95c9b66976f3b20494
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:53:57 +0300

    Update duplicate-manifest.sql

--- DSL/Resql/centops/POST/duplicate-manifest.sql
+++ DSL/Resql/centops/POST/duplicate-manifest.sql
@@ -1,16 +1,19 @@
-WITH origin AS (SELECT client_id, name, helm_version, helm_values
+WITH origin AS (SELECT client_id, name, git_helm_branch, helm_values, git_helm_repository, git_helm_path
                 FROM manifests
                 WHERE client_id = :client_id::uuid
-                  AND manifest_id = CAST(:manifest_id AS BIGINT)
+                  AND manifest_id =:manifest_id::uuid
                   AND deleted = false
                 LIMIT 1),
      inserted AS (
-         INSERT INTO manifests (client_id, name, helm_version, helm_values, created_at)
+         INSERT INTO manifests (client_id, name, git_helm_branch, helm_values, created_at, git_helm_repository, git_helm_path, manifest_id)
              SELECT origin.client_id,
                     CONCAT(origin.name, ' Copy ', TO_CHAR(NOW(), 'YYYY-MM-DD HH24:MI:SS')),
-                    origin.helm_version,
+                    origin.git_helm_branch,
                     origin.helm_values,
-                    NOW()
+                    NOW(),
+                    origin.git_helm_repository,
+                    origin.git_helm_path,
+                    uuid_generate_v4()
              FROM origin
              RETURNING manifest_id)
 SELECT manifest_id
