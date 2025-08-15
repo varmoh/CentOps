2e9c831ea991c7be4c2ef2be762b3d802e503c0a
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-15 06:56:49 +0300

    Update update-manifest.sql

--- DSL/Resql/centops/POST/update-manifest.sql
+++ DSL/Resql/centops/POST/update-manifest.sql
@@ -1,19 +1,22 @@
 WITH origin AS (
     UPDATE manifests
         SET deleted = true
-        WHERE client_id = :client_id::uuid AND manifest_id = CAST(:manifest_id AS BIGINT)
-        RETURNING created_at, deployed_at
+        WHERE client_id = :client_id::uuid AND manifest_id = :manifest_id::uuid
+        RETURNING created_at, deployed_at, manifest_id
 ),
      inserted AS (
-         INSERT INTO manifests (client_id, name, helm_version, helm_values, created_at, updated_at, deployed_at)
+         INSERT INTO manifests (client_id, manifest_id, name, git_helm_branch, helm_values, created_at, updated_at, deployed_at, git_helm_repository, git_helm_path)
              SELECT
-                 :client_id::uuid,
+                :client_id::uuid,
+                 origin.manifest_id,
                  :name,
-                 :helm_version,
+                 :git_helm_branch,
                  :helm_values,
                  origin.created_at,
                  NOW(),
-                 origin.deployed_at
+                 origin.deployed_at,
+                 :git_helm_repository,
+                 :git_helm_path
              FROM origin
              RETURNING manifest_id
      )
