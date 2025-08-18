774ec41f8e0df6490291ec6697dd01445d3844f2
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-18 15:11:48 +0300

    Update get-deployments-by-client-id.sql

--- DSL/Resql/centops/POST/get-deployments-by-client-id.sql
+++ DSL/Resql/centops/POST/get-deployments-by-client-id.sql
@@ -1,5 +1,6 @@
 SELECT id,
        client_id,
+       deployment_id,
        manifest_id,
        manifest_name,
        manifest_git_helm_branch,
@@ -8,8 +9,9 @@ SELECT id,
        concat(deployed_by_username, ' ', deployed_by_lastname) as deployed_by,
        status,
        created_at,
-       CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
-FROM deployments
-WHERE client_id = :client_id::uuid
+       CEIL(COUNT(*) OVER() / :page_size::DECIMAL)             AS total_pages
+FROM deployments d
+WHERE id = (SELECT max(id) FROM deployments WHERE deployment_id = d.deployment_id AND client_id = :client_id::uuid)
+  AND deleted = FALSE
 ORDER BY created_at DESC
 OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
