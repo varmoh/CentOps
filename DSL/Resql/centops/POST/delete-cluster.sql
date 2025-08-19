b92d2c9c2728c706b93a547cc5c7643bd40750ef
Varmo <101868197+varmoh@users.noreply.github.com>
2025-08-19 08:37:16 +0300

    Update delete-cluster.sql

--- DSL/Resql/centops/POST/delete-cluster.sql
+++ DSL/Resql/centops/POST/delete-cluster.sql
@@ -2,4 +2,5 @@ INSERT INTO clusters (cluster_id, deleted)
 SELECT cluster_id, true
 FROM clusters
 WHERE cluster_id = :cluster_id::uuid
-  AND id = (SELECT max(id) FROM clusters WHERE cluster_id = :cluster_id::uuid);
+  AND id = (SELECT max(id) FROM clusters WHERE cluster_id = :cluster_id::uuid)
+  AND deleted = false;
