7c6b9586cd7c5322ff5968f1635ab28b2315ccae
Varmo <101868197+varmoh@users.noreply.github.com>
2025-09-23 08:46:13 +0300

    Update validate-api-client.sql

--- DSL/Resql/centops/POST/validate-api-client.sql
+++ DSL/Resql/centops/POST/validate-api-client.sql
@@ -1,19 +1,5 @@
-WITH credentials AS (SELECT split_part(
-                                    convert_from(
-                                            decode(:authorization::text, 'base64'),
-                                            'utf8'
-                                    ),
-                                    ':', 1
-                            ) AS api_key,
-                            split_part(
-                                    convert_from(
-                                            decode(:authorization, 'base64'),
-                                            'utf8'
-                                    ),
-                                    ':', 2
-                            ) AS api_secret)
 SELECT EXISTS (SELECT 1
                FROM api_clients
-               WHERE api_key = (SELECT api_key FROM credentials)
-                 AND api_secret = (SELECT api_secret FROM credentials)
-                 AND is_enabled = true) ::text AS is_valid;
+               WHERE api_key = :api_key
+                 and api_secret = :api_secret
+                 and is_enabled = true) AS is_valid;
